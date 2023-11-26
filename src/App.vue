6a246530a6abff9eb4a007e3ee0b54f0511fd8da
Jhosep Tinta <tmjhosep@gmail.com>
2023-11-25 21:30:50 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts" setup>
+  import AppBar from '@/layouts/default/AppBar.vue'
+</script>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-4" />
+      Nav Bar
+      <v-btn class="ml-6" text to="/">Inicio</v-btn>
+      <v-btn text to="/pagina1">Página 1</v-btn>
+      <v-btn text to="/pagina2">Página 2</v-btn>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
